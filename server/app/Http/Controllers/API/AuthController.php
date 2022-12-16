7557a70b2a814d65e8569059fbd05011086f0ffd
abdulkader-safi <safi.abdulkader@gmail.com>
2022-12-16 20:11:05 +0200

    Add Login && Logout for new Users

--- server/app/Http/Controllers/API/AuthController.php
+++ server/app/Http/Controllers/API/AuthController.php
@@ -27,11 +27,17 @@ class AuthController extends Controller
                 'name' => ['required', 'string', 'min:2', 'max:191', 'unique:users'],
                 'email' => ['required', 'string', 'unique:users', 'email'],
                 'password' => ['required', 'string', 'confirmed', 'min:8'],
-                'password_verify' => ['required', 'string', 'confirmed', 'min:8']
             ]
         );
 
-        if ($req->password === $req->password_verify) {
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => 400,
+                'massage' => $validator->messages()
+            ]);
+        }
+
+        if ($req->password === $req->password_confirmation) {
             $user = User::create([
                 'name' => $req->name,
                 'email' => $req->email,
@@ -39,18 +45,74 @@ class AuthController extends Controller
             ]);
         } else {
             return response()->json([
-                "status" => 401,
-                "massage" => "Password and Password Verification does not match"
+                "status" => 400,
+                "massage" => "Password and Password Confirmation does not match"
             ]);
         }
 
-        $token = $user->createToken('MyAppToken')->plainTextToken;
+        $token = $user->createToken($user->email . '_Token', [''])->plainTextToken;
 
         return response()->json([
             "status" => 200,
             "massage" => "User Created Successfully",
             "token" => $token,
             "data" => $user
+        ]);
+    }
+
+    /**
+     * login to  User
+     * @param  Illuminate\Http\Request $req
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function login(Request $req): JsonResponse
+    {
+        $validator = Validator::make(
+            $req->all(),
+            [
+                'email' => ['required', 'string', 'email'],
+                'password' => ['required', 'string', 'min:8'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => 400,
+                'massage' => $validator->messages()
+            ]);
+        }
+
+        $user = User::where('email', $req->email)->first();
+
+        if (!$user || !Hash::check($req->password, $user->password)) {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Invalid Credentials'
+            ]);
+        }
+        $token = $user->createToken($user->email . '_Token', [''])->plainTextToken;
+
+
+        return response()->json([
+            'status' => 200,
+            'message' => "Login In Successfully",
+            'username' => $user->name,
+            'token' => $token,
+        ]);
+    }
+
+    /**
+     * login to  User
+     * @param  Illuminate\Http\Request $req
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => "Logged Out Successfully"
         ]);
     }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -5,14 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::name('user.')
-    ->prefix('user')
-    ->group(function () {
-        Route::post('/register', [AuthController::class, 'register']);
-    });
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('user/register', [AuthController::class, 'register']);
+Route::post('user/login', [AuthController::class, 'login']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('user/logout', [AuthController::class, 'logout']);
+});
