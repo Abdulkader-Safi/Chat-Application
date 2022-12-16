73af976d7e18a9e99075c3416f7b0fb654cf7297
abdulkader-safi <safi.abdulkader@gmail.com>
2022-12-16 22:35:08 +0200

    Setup API BASE URL && setup TialwindCSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+
+axios.defaults.withCredentials = true;
+
+// axios.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("auth_token");
+//   config.headers.Authorization = token ? `Bearer ${token}` : "";
+//   return config;
+// });
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
+  return (
+    <div className=''>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+
+}
 export default App
